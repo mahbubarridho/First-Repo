7ad51c3ac7d92ef977f772958b60b505dd5ca505
mahbubarridho <mbubmahbub9@gmail.com>
2026-02-15 21:29:13 -0800

    [MODIFY] buat file aritmatika main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+print('operations func')
 a = 1
 b = 2
 
-result = a + b
+print('kita memiliki 2 variable a dan b')
+print('a = 1')
+print('b = 2')
 
-print(result)
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a * b
+
+print('hasil penjumlahan a dan b =>', penjumlahan)
+print('hasil pengurangan a dan b =>', pengurangan)
+print('hasil perkalian a dan b =>', perkalian)
+
+print('')
+print('If Else')
+
+usia = 19
+
+# pengkondisian
+if usia >= 12 | usia <= 19:
+    print('remaja')
